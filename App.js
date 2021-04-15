558c9b97e87f24f8a8194ab1ac0eb7f838000216
Sneha <singhsneha.july@gmail.com>
2021-04-15 22:54:47 +0530

    deeplinking in IOS configure

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import {StyleSheet, View, Text} from 'react-native';
 import {Root} from 'native-base';
 import AppRouter from './src/navigation/Routes';
 import {MainDrawerNavigation} from './src/navigation/MainDrawerNavigation';
-import {RootNavigator} from './src/navigation/CombinedNavigation';
+import {CombinedNavigation} from './src/navigation/CombinedNavigation';
+import {DrawerNavigator} from './src/navigation/DrawerNavigator';
 
 const App = () => {
   return (
     <Root>
       {/* <AppRouter /> */}
       {/* <MainDrawerNavigation /> */}
-      <RootNavigator />
+      {/* <CombinedNavigation /> */}
+      <DrawerNavigator />
     </Root>
   );
 };
