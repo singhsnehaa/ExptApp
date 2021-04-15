3cdb8f97e55a4bcb453908ee53e4f4cad77fb0b4
Sneha <singhsneha.july@gmail.com>
2021-04-15 22:48:28 +0530

    Created Combined navigation in single page wit custome tab navigation

--- src/navigation/CombinedNavigation.js
+++ src/navigation/CombinedNavigation.js
@@ -4,6 +4,10 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {COLORS} from '../common/Colors';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const HomeStack = createStackNavigator();
 const ProfileStack = createStackNavigator();
@@ -13,7 +17,7 @@ const Drawer = createDrawerNavigator();
 function Home(props) {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home Page</Text>
+      <Text>Home Page </Text>
       <TouchableOpacity
         onPress={() => props.navigation.navigate('HomeDetails')}>
         <Text>Go to Home Details</Text>
@@ -86,15 +90,79 @@ function ProfileStackContainer() {
 
 function TabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeStackContainer} />
-      <Tab.Screen name="Pofile" component={ProfileStackContainer} />
-      <Tab.Screen name="Setting" component={Setting} />
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: COLORS.WARNING,
+        inactiveTintColor: 'gray',
+        showLabel: false,
+
+        // activeTabStyle: {
+        //   fontWeight: 'bold',
+        //   backgroundColor: 'red',
+        // },
+        //activeBackgroundColor: 'yellow',
+        style: {
+          height: 64,
+          marginBottom: 10,
+          borderRadius: 50,
+          marginLeft: 40,
+          marginRight: 40,
+          backgroundColor: '#33cc33',
+        },
+        tabStyle: {
+          borderLeftWidth: 1,
+          borderLeftColor: '#fff',
+
+          //paddingTop: 10,
+          //borderTopColor: '#3A3AB5',
+          //borderTopWidth: 1,
+          //height: 64,
+          //borderTopWidth: 0,
+          // backgroundColor: 'transparent',
+          //elevation: 30,
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeStackContainer}
+        options={{
+          tabBarIcon: ({color, size, focused}) => (
+            <FontAwesome
+              size={30}
+              name={focused ? 'home' : 'comment-o'}
+              color={COLORS.WHITE}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pofile"
+        component={ProfileStackContainer}
+        options={{
+          tabBarIcon: ({color, size, focused}) => (
+            <MaterialCommunityIcons
+              size={30}
+              name="face-profile"
+              color={COLORS.WHITE}
+            />
+          ),
+          tabBarBadge: 3,
+        }}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={Setting}
+        options={{
+          tabBarIcon: ({color, size, focused}) => (
+            <MaterialIcons size={30} name="settings" color={COLORS.WHITE} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
 
-export function RootNavigator() {
+export function CombinedNavigation() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
@@ -104,3 +172,11 @@ export function RootNavigator() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  bottomBarStyle: {
+    paddingTop: 10,
+    borderTopColor: '#3A3AB5',
+    borderTopWidth: 100,
+  },
+});
