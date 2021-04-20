a1e16a4fa5d03c1c04ea49562ea8ab37c840c7ed
Sneha <singhsneha.july@gmail.com>
2021-04-20 16:03:33 +0530

    Some basic improvment

--- src/pages/HomeDetails.js
+++ src/pages/HomeDetails.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Text, StyleSheet, View, TouchableOpacity} from 'react-native';
 
 import Header from '../common/Header';
 import Icons from '../common/Icons';
 import {COLORS} from '../common/Colors';
 
-function HomeDetails() {
-  return (
-    <>
-      <Header
-        title={'Home Details'}
-        backButton
-        headerStyle={{backgroundColor: COLORS.WARNING, zIndex: 100}}
-        titleStyle={{color: COLORS.WHITE}}
-      />
+export class HomeDetails extends Component {
+  componentDidMount = () => {
+    console.log('Home Details Page');
+  };
 
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{fontSize: 25, color: COLORS.WARNING}}>Home Details</Text>
-      </View>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <Header
+          title={'Home Details'}
+          backButton
+          headerStyle={{backgroundColor: COLORS.WARNING, zIndex: 100}}
+          titleStyle={{color: COLORS.WHITE}}
+        />
+
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{fontSize: 25, color: COLORS.WARNING}}>
+            Home Details
+          </Text>
+        </View>
+      </>
+    );
+  }
 }
 
 export default HomeDetails;
