17b62ec1af3e9f198813e29946374d27b9b72ab6
Sneha <singhsneha.july@gmail.com>
2021-05-12 12:21:30 +0530

    updated main Drawer

--- src/navigation/MainDrawerNavigation.js
+++ src/navigation/MainDrawerNavigation.js
@@ -5,6 +5,7 @@ import {ContactList} from '../screens/ContactList';
 import {UploadResume} from '../screens/UploadResume';
 import {VideoList} from '../screens/VideoList';
 import {AppleSignIn} from '../screens/AppleSignIn';
+import {TooltipExmpl} from '../screens/TooltipExmpl';
 import {SigninGoogle} from '../screens/SigninGoogle';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
@@ -13,9 +14,10 @@ const Drawer = createDrawerNavigator();
 export function MainDrawerNavigation() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="SigninGoogle">
+      <Drawer.Navigator initialRouteName="TooltipExmpl">
         <Drawer.Screen name="SigninGoogle" component={SigninGoogle} />
         <Drawer.Screen name="AppleSignIn" component={AppleSignIn} />
+        <Drawer.Screen name="TooltipExmpl" component={TooltipExmpl} />
         <Drawer.Screen name="HomeScreen" component={HomeScreen} />
         <Drawer.Screen name="VideoList" component={VideoList} />
         <Drawer.Screen name="ContactList" component={ContactList} />
