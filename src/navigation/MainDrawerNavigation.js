5fac5602448f9b60d00b6c3e2702254e068e0c12
Sneha <singhsneha.july@gmail.com>
2021-05-21 17:09:44 +0530

    swipe view list completed

--- src/navigation/MainDrawerNavigation.js
+++ src/navigation/MainDrawerNavigation.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {View, Text} from 'react-native';
-import {IntroSlider} from '../screens/IntroSlider';
+import {SwipeList} from '../screens/SwipeList';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 const Drawer = createDrawerNavigator();
@@ -8,8 +8,8 @@ const Drawer = createDrawerNavigator();
 export function MainDrawerNavigation() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="IntroSlider">
-        <Drawer.Screen name="IntroSlider" component={IntroSlider} />
+      <Drawer.Navigator initialRouteName="SwipeList">
+        <Drawer.Screen name="SwipeList" component={SwipeList} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
